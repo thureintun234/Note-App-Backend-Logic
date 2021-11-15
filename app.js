a9ec2f4eb54570ace781e64efd11e1539899de72
thureintun234 <thurein12456@gmail.com>
2021-11-15 21:42:04 +0630

    final

---
+++ app.js
@@ -0,0 +1,39 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const notesRouter = require('./controllers/note')
+const usersRouter = require('./controllers/user.js')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connectiong to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to mongodb')
+  })
+  .catch(err => {
+    logger.error('error connecting to mongodb', err.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ utils/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.NODE_EVN === 'test' ?
+  process.env.TEST_MONGODB_URI :
+  process.env.MONGODB_URI
+
+module.exports = {
+  PORT,
+  MONGODB_URI
+}
+
+
